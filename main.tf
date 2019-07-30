deb048ca7424135c87eaddde186c7e5f65c710bd
callumccr1 <callumccr@gmail.com>
2019-07-30 16:24:00 +1200

    fixed ashby's shit

--- main.tf
+++ main.tf
@@ -1,3 +1,43 @@
+/*****
+Common: Remote state backend
+*****/
 terraform {
-  backend "azurerm" {}
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+  required_version = ">= 0.12"
+}
+
+/*****
+Modules: tf-proj-auzre-baseline - "git@github.com:mashbynz/tf-proj-azure-baseline?ref=master"
+*****/
+
+module "label" {
+  source             = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
+  namespace          = var.namespace
+  environment        = var.environment
+  name               = var.name
+  attributes         = var.attributes
+  delimiter          = "-"
+  additional_tag_map = {} /* Additional attributes (e.g. 1) */
+  label_order        = ["environment", "namespace", "name", "attributes"] /* Default label order */
+  tags = {
+    "project" = var.project
+  }
+}
+
+module "vnet" {
+  source                = "git@github.com:mashbynz/tf-mode-azure-vnet?ref=master"
+  context               = module.label.context
+  region                = var.region
+  ssvnet_addressspace   = var.ssvnet_addressspace
+  ssvnetddos_name       = var.ssvnetddos_name
+  gatewaynsg_name       = var.gatewaynsg_name
+  gatewaysubnet_name    = var.gatewaysubnet_name
+  gatewaysubnet_prefix  = var.gatewaysubnet_prefix
+  firewallsubnet_name   = var.firewallsubnet_name
+  firewallsubnet_prefix = var.firewallsubnet_prefix
+  GatewayRT_name        = var.GatewayRT_name
+  GatewayRT_prefix      = var.GatewayRT_prefix
+  GatewayRT_nexthoptype = var.GatewayRT_nexthoptype
+  GatewayRT_nexthopIP   = var.GatewayRT_nexthopIP
 }
---
+++ variables.tf
@@ -0,0 +1,179 @@
+/*****
+Common Variables: Azure provider - Autoloaded from Terragrunt.
+*****/
+
+variable "region" {
+  description = "The Azure region (e.g. australia east). Autoloaded from region.tfvars."
+  type        = string
+  default     = ""
+}
+
+variable "subscription_id" {
+  description = "The Azure Subscription ID (e.g. 7e719999-342b-44a8-9cd2-eada38cb7f08). Autoloaded from subscription.tfvars."
+  type        = string
+  default     = ""
+}
+
+variable "tenant_id" {
+  description = "The Azure Tenant ID (e.g. a4d44117-bb20-4668-a0dc-16fcb91100b7). Autoloaded from subscription.tfvars."
+  type        = string
+  default     = ""
+}
+
+variable "remote_state_blob" {
+  description = "The Azure remote state blob. Autoloaded from the parent terragrunt.hcl"
+  type        = string
+  default     = ""
+}
+
+
+variable "remote_state_blob_region" {
+  description = "The Azure remote state blob region. Autoloaded from the parent terragrunt.hcl"
+  type        = string
+  default     = ""
+}
+
+/*****
+Label Module Variables - "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
+*****/
+
+variable "namespace" {
+  type        = string
+  default     = ""
+  description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
+}
+
+variable "namespace" {
+  type        = string
+  default     = ""
+  description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
+}
+
+variable "costcentre" {
+  type        = string
+  default     = ""
+  description = "Costcentre, which could be the cost centre or entity, e.g. 'ICT' or 'NZTA'"
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
+}
+
+variable "name" {
+  type        = string
+  default     = ""
+  description = "Solution name, e.g. 'app' or 'baseline'"
+}
+
+variable "project" {
+  type        = string
+  default     = ""
+  description = "the internal project name"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
+}
+
+/*****
+VNET Module Variables - https://github.com/mashbynz/tf-mod-azure-vnet
+@Matt Ashby To Fill In
+*****/
+
+/*****
+VNET Module Variables - https://github.com/mashbynz/tf-mod-azure-vnet
+@Matt Ashby To Fill In
+*****/
+
+variable "rg_name" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "vnet_name" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "vnet_addressspace" {
+  type        = list(string)
+  description = ""
+  default     = []
+}
+
+variable "vnet_ddos_name" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "gateway_nsg_name" {
+  type        = string
+  description = ""
+  default     = ""
+}
+variable "gateway_subnet_name" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "gateway_subnet_prefix" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "firewall_subnet_name" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "firewall_subnet_prefix" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "gateway_rt_name" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "gateway_rt_prefix" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "gateway_rt_nexthop_type" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "gateway_rt_nexthop_ip" {
+  type        = string
+  description = ""
+  default     = ""
+}
