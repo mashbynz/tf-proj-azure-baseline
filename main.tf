3d772c61152db69c4885973e8cb23fd3109eef88
Matt Ashby <matt.ashby@theinstillery.com>
2019-08-01 14:44:24 +1200

    label update

--- main.tf
+++ main.tf
@@ -36,11 +36,11 @@ module "paaslabel" {
   source             = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
   namespace          = var.namespace
   environment        = var.environment
-  name               = var.analytics_name
+  name               = var.environment + var.analytics_name
   attributes         = var.attributes
   delimiter          = ""
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
-  label_order        = ["environment", "name", "attributes"] /* Default label order */
+  label_order        = ["name", "attributes"] /* Default label order */
   tags = {
     "project"    = var.project
     "costcenter" = var.costcentre
