c37d0b811e463722e81d0fb8e1323295914e57fd
Matt Ashby <matt.ashby@theinstillery.com>
2019-08-09 14:29:11 +1200

    context update

--- main.tf
+++ main.tf
@@ -125,14 +125,14 @@ module "paas" {
 
 module "subscription_owner" {
   source           = "git::https://github.com/mashbynz/tf-mod-azure-iam.git?ref=master"
-  context          = module.paaslabel.context
+  # context          = module.paaslabel.context
   assignable_scope = var.sub_owner
   ad_group_id      = var.sub_owner_ad_group_id
 }
 
 module "subscription_reader" {
   source           = "git::https://github.com/mashbynz/tf-mod-azure-iam.git?ref=master"
-  context          = module.paaslabel.context
+  # context          = module.paaslabel.context
   assignable_scope = var.sub_reader
   ad_group_id      = var.sub_reader_ad_group_id
 }
